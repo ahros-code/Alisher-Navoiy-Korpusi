7b7c570042475c7561257771f3f8db70f3acfae8
ahros-code <probroalpha@gmail.com>
2024-06-27 13:10:20 +0500

    little bugfix

--- src/context/BaytContext.jsx
+++ src/context/BaytContext.jsx
@@ -3,7 +3,7 @@ import {createContext, useState} from "react";
 export const BaytContext = createContext();
 
 export const BaytContextProvider = ({children}) => {
-    const [selectedBayt, setSelectedBayt] = useState();
+    const [selectedBayt, setSelectedBayt] = useState({});
     return (
         <BaytContext.Provider value={{selectedBayt, setSelectedBayt}}>
             {children}
